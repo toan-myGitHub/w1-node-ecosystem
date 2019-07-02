d6a880f12b7084b510c7754877f3e729399438c7
toanhbui <toanhbui@uw.edu>
2019-07-02 13:55:01 -0700

    0702

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Hello, Node!')
+const profile = require('./node_modules/moment/src/profile')
+console.log(profile)
+
+const path = require('path')
+console.log(path.resolve())
+
+const moment = require('./node_modules/moment/moment.js')
+/*this also works 
+const moment = require('moment')
+node knows where to look
+*/
+
+const timenow = moment().format('LTS')
+console.log(timenow)
